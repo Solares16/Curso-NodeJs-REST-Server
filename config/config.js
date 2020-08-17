b8eff9195056cebfce99c5c95372815dea97b1b2
Solares <Solares@LAPTOP-U4OFT358>
2020-08-17 13:39:59 -0600

    variable de entorno MONGO_URI

--- config/config.js
+++ config/config.js
@@ -24,7 +24,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://solaresdb:5peNgVzC68cSJ55E@cluster0.vyzjj.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
