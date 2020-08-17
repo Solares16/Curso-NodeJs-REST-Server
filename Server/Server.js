4910775afbe25e62544c223bf24267fa986b87a9
Solares <Solares@LAPTOP-U4OFT358>
2020-08-17 13:14:46 -0600

    RestServer - Usuario

--- Server/Server.js
+++ Server/Server.js
@@ -1,7 +1,8 @@
 require('../config/config');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 
 
@@ -11,50 +12,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
 
-    res.json('get usuario');
+app.use(require('./routes/usuario'));
 
-});
-
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
+mongoose.connect(process.env.URLDB, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    },
+    (err, res) => {
+        if (err) throw err;
+        console.log('DataBase online')
     });
-
-});
-
-app.delete('/usuario', function(req, res) {
-
-    res.json('delete usuario');
-
-});
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuhando en el puerto: `, process.env.PORT);
 });
+
+
+//mLab credentials
+//user: solaresdb
+//pass: 5peNgVzC68cSJ55E
+
+//String connection: mongodb+srv://solaresdb:5peNgVzC68cSJ55E@cluster0.vyzjj.mongodb.net/cafe
--- config/config.js
+++ config/config.js
@@ -5,4 +5,28 @@
  */
 
 
-process.env.PORT = process.env.PORT || 3015;
+process.env.PORT = process.env.PORT || 3000;
+
+/** 
+ * 
+ * 
+ * ENTORNO
+ */
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+/**
+ * BASE DE DATOS
+ */
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://solaresdb:5peNgVzC68cSJ55E@cluster0.vyzjj.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
+//local:  'mongodb: //localhost:27017/cafe'
+//remota: 'mongodb + srv: //solaresdb:5peNgVzC68cSJ55E@cluster0.vyzjj.mongodb.net/cafe'
